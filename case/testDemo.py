d8a2a35ffe7453b7cb5e81e39dc8e6b275ac948a
AUTHENTICGIT <leogwork@gmail.com>
2017-10-23 15:12:31 +0800

    Modify Sleep Time

--- case/testDemo.py
+++ case/testDemo.py
@@ -8,13 +8,13 @@ class Login(unittest.TestCase):
 
         # 窗口最大化
         self.driver.maximize_window()
-        self.msg = 'selenium 定位元素'
+        self.msg = '王牌特工2'
         self.url = 'http://www.baidu.com'
 
     def testSearch(self):
         # open browser
         self.driver.get(self.url)
-        sleep(3)
+        sleep(2)
         # click search input
         self.driver.find_element_by_id('kw').click()
         sleep(1)
@@ -23,8 +23,9 @@ class Login(unittest.TestCase):
         self.driver.find_element_by_id('kw').send_keys(self.msg)
         sleep(1)
         self.driver.find_element_by_id('su').click()
-        sleep(1)
+        sleep(3)
     def tearDown(self):
         self.driver.close()
 if __name__ == "__main__":
     unittest.main()
+
