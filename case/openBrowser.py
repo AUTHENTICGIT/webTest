40c2a27e4aeeafd4b410cddc3689cd1ca5e54604
AUTHENTICGIT <leogwork@gmail.com>
2017-11-01 17:59:40 +0800

    H5 Weibo

--- case/openBrowser.py
+++ case/openBrowser.py
@@ -1,27 +1,52 @@
 from selenium import webdriver
+from time import sleep
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class Driver:
-    def __init__(self, url):
-        self.browser = webdriver.Chrome()
-        self.url = url
-    def open_browser(self):
-        """
-        Do something for browser
-        :return: browser
-        """
-        # 窗口最大化
-        self.browser.maximize_window()
-
-        # 打开地址链接
-        self.browser.get(self.url)
-        return self.browser
-    def close_browser(self):
-        """
-        quit browser
-        :return:
-        """
-        self.browser.quit()
-
-url = input('Enter the website:')
-d = Driver(url)
-d.open_browser()
+    def __init__(self):
+        self.driver = webdriver.Firefox()
+        self.url = 'https://passport.weibo.cn/signin/login'
+        self.account = 'leogwork@gmail.com'
+        self.pwd = 'ksxyhzgdnz007'
+    def open(self):
+        self.driver.get(self.url)
+        sleep(3)
+        # 账户
+        print(">>>登入账户>>>")
+        self.driver.find_element_by_id("loginName").send_keys(self.account)
+        sleep(1)
+        # 密码
+        print(">>>登入密码>>>")
+        self.driver.find_element_by_id("loginPassword").send_keys(self.pwd)
+        sleep(1)
+        # 登录
+        self.driver.find_element_by_id("loginAction").click()
+        sleep(1)
+        print(">>>登录成功>>>")
+        sleep(3)
+        # zy主页
+        self.driver.get("https://m.weibo.cn/status/4169032489925104")
+        print(">>>跳转指定微博>>>")
+        sleep(5)
+        for i in range(50):
+            # 转发微博
+            print(">>>打开转发框>>>")
+            self.driver.find_element_by_xpath("/html/body/div/div[1]/div[2]/div/div/footer/div[1]").click()
+            sleep(2)
+            print(">>>转发语>>>")
+            self.driver.find_element_by_xpath("/html/body/div/div[1]/div/main/div[1]/div/span/textarea[1]").send_keys("[TEST]Auto-Forward Script No." + str(i+1))
+            sleep(2)
+            # 选择公开/私密
+            self.driver.find_element_by_xpath("/html/body/div/div[1]/div/footer/div[1]/div[2]").click()
+            sleep(2)
+            # 发送
+            self.driver.find_element_by_xpath("/html/body/div/div[1]/div/header/div[3]/a").click()
+            print(">>>转发第" + str(i+1) +"条>>>")
+            sleep(4)
+        print("\n脚本结束！共转发" + str(i+1) +"条微博")
+    def close(self):
+        self.driver.close()
+driver = Driver()
+driver.open()
