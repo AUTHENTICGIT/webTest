b55489bf38c205c9145271dab401d93c52500f62
AUTHENTICGIT <leogwork@gmail.com>
2017-11-01 18:02:04 +0800

    Initial commit

--- case/qunar.py
+++ case/qunar.py
@@ -4,19 +4,28 @@ import unittest
 
 class Driver(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Chrome()
-        self.driver.maximize_window()
+        self.driver = webdriver.Firefox()
+        # self.driver.maximize_window()
         self.url = 'https://flight.qunar.com'
         self.fromCity = '成都'
         self.toCity = '上海'
     def testSearch(self):
         self.driver.get(self.url)
         sleep(3)
+        # 选择出发城市：成都
         self.driver.find_element_by_name('fromCity').click()
         sleep(1)
-        self.driver.find_element_by_name('fromCity').send_keys(self.fromCity)
+        self.driver.find_element_by_xpath("/html/body/div[5]/div/div[2]/div/div/div/div[3]/div[1]/form/div[2]/div[1]/div/div[2]/div/div[2]/div[2]/dl/dd/ul/li[3]/a").click()
         sleep(1)
-
+        # 选择到达城市：上海
+        self.driver.find_element_by_name('toCity').click()
+        sleep(1)
+        self.driver.find_element_by_xpath("/html/body/div[5]/div/div[2]/div/div/div/div[3]/div[1]/form/div[2]/div[2]/div/div[2]/div/div[2]/div[2]/dl[1]/dd/ul/li[1]/a").click()
+        sleep(1)
+        # 选择today+7日后的日期
+        # self.driver.find_element_by_name('fromDate').click()
+        self.driver.find_element_by_class_name('btn_search').click()
+        sleep(10)
     def tearDown(self):
         self.driver.close()
 
