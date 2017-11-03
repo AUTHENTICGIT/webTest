4175b8836b32f4000d40c78c8a28828af266da2c
AUTHENTICGIT <leogwork@gmail.com>
2017-11-02 20:22:42 -0400

    Auto-Forward Sina Weibo

--- case/forwardWB1st.py
+++ case/forwardWB1st.py
@@ -5,10 +5,12 @@ import os
 
 class Driver:
     def __init__(self):
-        self.driver = webdriver.Firefox()
+        self.driver = webdriver.Chrome()
         self.url = 'https://passport.weibo.cn/signin/login'
         self.account = '13093870429'
         self.pwd = 'Akira0429'
+        self.tag, self.emoji = self.words()
+
     def open(self):
         self.driver.get(self.url)
         sleep(3)
@@ -26,33 +28,45 @@ class Driver:
         print(">>>登录成功>>>")
         sleep(3)
         # 定位第一条微博
-        self.driver.find_element_by_xpath("/html/body/div[1]/div[1]/div[2]/div[1]/div[2]/section/div[1]/div[2]/section").click()
-        sleep(3)
-        for i in range(5):
+        # self.driver.find_element_by_xpath("/html/body/div[1]/div[1]/div[2]/div[1]/div[2]/section/div[1]/div[2]/section").click()
+        self.driver.get("https://m.weibo.cn/status/4169399802125047")
+        sleep(2)
+        for i in range(59,70):
             # 转发微博
             print(">>>打开转发框>>>")
             self.driver.find_element_by_xpath("/html/body/div/div[1]/div[2]/div/div/footer/div[1]").click()
             sleep(2)
             print(">>>转发语>>>")
-            self.driver.find_element_by_xpath("/html/body/div/div[1]/div/main/div[1]/div/span/textarea[1]").send_keys('关键字', str(i+1))
-            sleep(2)
+            self.driver.find_element_by_xpath("/html/body/div/div[1]/div/main/div[1]/div/span/textarea[1]").send_keys(random.choice(self.tag),random.choice(self.emoji),str(i+1))
+            sleep(1)
+            self.driver.find_element_by_xpath('//*[@id="app"]/div[1]/div[2]/div[1]/div').click()
+            sleep(1)
+            self.driver.find_element_by_xpath('//*[@id="app"]/div[1]/div[2]/div[1]/div/div/div/div').click()
+            sleep(1)
             # 选择公开/私密
             # self.driver.find_element_by_xpath("/html/body/div/div[1]/div/footer/div[1]/div[2]").click()
             # sleep(2)
             # 发送
             self.driver.find_element_by_xpath("/html/body/div/div[1]/div/header/div[3]/a").click()
+            # self.driver.find_element_by_xpath('//*[@id="app"]/div[1]/div/header/div[3]/a').click()
             print(">>>转发第" + str(i+1) +"条>>>")
-            sleep(4)
+            sleep(5)
         print("\n脚本结束！共转发" + str(i+1) +"条微博")
     def words(self):
         os.chdir('../file')
-        fh = open('Tag_emoji.txt', 'r')
-        lines = fh.readlines()
-        tags = list()
-        for line in lines:
-            print(line)
-            tags.append(line)
-        return tags
+        fh1 = open('Tag_name.txt', 'r')
+        fh2 = open('Tag_emoji.txt', 'r')
+        lines1 = fh1.readlines()
+        lines2 = fh2.readlines()
+        words_tag = list()
+        words_emoji = list()
+        for line in lines1:
+            line = line.rstrip()
+            words_tag.append(line)
+        for line in lines2:
+            line = line.rstrip()
+            words_emoji.append(line)
+        return words_tag, words_emoji
 
     def close(self):
         self.driver.close()
