1f51c1538923b7b9a9490730c5ac846273894b88
AUTHENTICGIT <leogwork@gmail.com>
2017-11-20 14:20:16 +0800

    Initial commit

--- case/qunar.py
+++ case/qunar.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from time import sleep
 import unittest
+import datetime
 
 class Driver(unittest.TestCase):
     def setUp(self):
@@ -9,6 +10,7 @@ class Driver(unittest.TestCase):
         self.url = 'https://flight.qunar.com'
         self.fromCity = '成都'
         self.toCity = '上海'
+        self.date = str(datetime.date.today() + datetime.timedelta(days=7))
     def testSearch(self):
         self.driver.get(self.url)
         sleep(3)
@@ -23,7 +25,9 @@ class Driver(unittest.TestCase):
         self.driver.find_element_by_xpath("/html/body/div[5]/div/div[2]/div/div/div/div[3]/div[1]/form/div[2]/div[2]/div/div[2]/div/div[2]/div[2]/dl[1]/dd/ul/li[1]/a").click()
         sleep(1)
         # 选择today+7日后的日期
-        # self.driver.find_element_by_name('fromDate').click()
+        self.driver.find_element_by_name('fromDate').clear()
+        self.driver.find_element_by_name('fromDate').send_keys(self.date)
+        # 搜索
         self.driver.find_element_by_class_name('btn_search').click()
         sleep(10)
     def tearDown(self):
