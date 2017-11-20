b8d258c8010f262da5d91b495024dbb3c32460a6
AUTHENTICGIT <leogwork@gmail.com>
2017-11-20 14:24:02 +0800

    Initial commit

--- case/forwardWB.py
+++ case/forwardWB.py
@@ -1,67 +1,75 @@
 from selenium import webdriver
 from time import sleep
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+import random
+import os
 
 class Driver:
     def __init__(self):
         self.driver = webdriver.Firefox()
-        self.url = 'https://weibo.com/'
-        self.account = 'leogwork@gmail.com'
-        self.pwd = 'ksxyhzgdnz007'
+        self.url = 'https://passport.weibo.cn/signin/login'
+        self.account = '13093870429'
+        self.pwd = 'Akira0429'
+        self.tag, self.emoji = self.words()
+
     def open(self):
         self.driver.get(self.url)
-        sleep(20)
+        sleep(3)
         # 账户
-        self.driver.find_element_by_id('loginname')
-        sleep(1)
         print(">>>登入账户>>>")
-        sleep(1)
-        self.driver.find_element_by_id('loginname').send_keys(self.account)
+        self.driver.find_element_by_id("loginName").send_keys(self.account)
         sleep(1)
         # 密码
-        self.driver.find_element_by_xpath("/html/body/div[1]/div[1]/div/div[2]/div[1]/div[2]/div/div[2]/div[1]/div[2]/div[1]/div/div/div/div[3]/div[2]/div/input")
-        sleep(1)
         print(">>>登入密码>>>")
-        sleep(1)
-        self.driver.find_element_by_css_selector(".password > div:nth-child(1) > input:nth-child(1)").send_keys(self.pwd)
+        self.driver.find_element_by_id("loginPassword").send_keys(self.pwd)
         sleep(1)
         # 登录
-        self.driver.find_element_by_xpath("/html/body/div[1]/div[1]/div/div[2]/div[1]/div[2]/div/div[2]/div[1]/div[2]/div[1]/div/div/div/div[3]/div[6]/a").click()
+        self.driver.find_element_by_id("loginAction").click()
         sleep(1)
         print(">>>登录成功>>>")
-        sleep(10)
-        # 个人主页
-        # self.driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div[3]/div[2]/div[2]/div[2]/div/div/div[1]/div/a/img").click()
-        # zy主页
-        self.driver.get("http://weibo.com/u/3668829440")
-        print(">>>定位转发主页>>>")
         sleep(3)
-        for i in range(10):
+        # 定位第一条微博
+        # self.driver.find_element_by_xpath("/html/body/div[1]/div[1]/div[2]/div[1]/div[2]/section/div[1]/div[2]/section").click()
+        self.driver.get("https://m.weibo.cn/status/4169399802125047")
+        sleep(2)
+        for i in range(59,70):
             # 转发微博
-            self.driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div/div[2]/div[2]/div[3]/div/div[3]/div[2]/div/ul/li[2]/a/span/span").click()
             print(">>>打开转发框>>>")
-            sleep(3)
-            self.driver.find_element_by_xpath("/html/body/div[10]/div[2]/div[3]/div/div[2]/div/div[2]/div/div[1]/div/div[1]/textarea").click()
+            self.driver.find_element_by_xpath("/html/body/div/div[1]/div[2]/div/div/footer/div[1]").click()
+            sleep(2)
             print(">>>转发语>>>")
-            sleep(3)
-            self.driver.find_element_by_xpath("/html/body/div[10]/div[2]/div[3]/div/div[2]/div/div[2]/div/div[1]/div/div[1]/textarea").send_keys("[TEST]Auto-Forward Script No." + str(i+1))
-            sleep(3)
+            self.driver.find_element_by_xpath("/html/body/div/div[1]/div/main/div[1]/div/span/textarea[1]").send_keys(random.choice(self.tag),random.choice(self.emoji),str(i+1))
+            sleep(1)
+            self.driver.find_element_by_xpath('//*[@id="app"]/div[1]/div[2]/div[1]/div').click()
+            sleep(1)
+            self.driver.find_element_by_xpath('//*[@id="app"]/div[1]/div[2]/div[1]/div/div/div/div').click()
+            sleep(1)
             # 选择公开/私密
-            self.driver.find_element_by_xpath("/html/body/div[10]/div[2]/div[3]/div/div[2]/div/div[2]/div/div[1]/div/div[2]/div[1]/div/a/i").click()
-            print(">>>选择公开/私密>>>")
-            sleep(3)
-            # 好友圈可见
-            self.driver.find_element_by_xpath("/html/body/div[10]/div[2]/div[3]/div/div[2]/div/div[2]/div/div[1]/div/div[2]/div[1]/div/div/ul/li[2]/a").click()
-            print(">>>好友圈可见>>>")
-            sleep(3)
-            # 好友发布
-            self.driver.find_element_by_xpath("/html/body/div[10]/div[2]/div[3]/div/div[2]/div/div[2]/div/div[1]/div/div[2]/div[1]/a").click()
+            # self.driver.find_element_by_xpath("/html/body/div/div[1]/div/footer/div[1]/div[2]").click()
+            # sleep(2)
+            # 发送
+            self.driver.find_element_by_xpath("/html/body/div/div[1]/div/header/div[3]/a").click()
+            # self.driver.find_element_by_xpath('//*[@id="app"]/div[1]/div/header/div[3]/a').click()
             print(">>>转发第" + str(i+1) +"条>>>")
-            sleep(3)
+            sleep(5)
         print("\n脚本结束！共转发" + str(i+1) +"条微博")
+    def words(self):
+        os.chdir('../file')
+        fh1 = open('Tag_name.txt', 'r')
+        fh2 = open('Tag_emoji.txt', 'r')
+        lines1 = fh1.readlines()
+        lines2 = fh2.readlines()
+        words_tag = list()
+        words_emoji = list()
+        for line in lines1:
+            line = line.rstrip()
+            words_tag.append(line)
+        for line in lines2:
+            line = line.rstrip()
+            words_emoji.append(line)
+        return words_tag, words_emoji
+
     def close(self):
         self.driver.close()
+
 driver = Driver()
 driver.open()
