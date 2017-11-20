b3086ef9af6f2e9c90205de460587f0659b9b333
AUTHENTICGIT <leogwork@gmail.com>
2017-11-20 16:40:53 +0800

    Initial commit

--- case/testDemo.py
+++ case/testDemo.py
@@ -2,30 +2,22 @@ from selenium import webdriver
 from time import sleep
 import unittest
 
-class Login(unittest.TestCase):
+class Driver(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Chrome()
-
-        # 窗口最大化
-        self.driver.maximize_window()
-        self.msg = '王牌特工2'
+        self.driver = webdriver.Firefox()
         self.url = 'http://www.baidu.com'
-
+        self.msg = '正义联盟'
     def testSearch(self):
         # open browser
         self.driver.get(self.url)
         sleep(2)
-        # click search input
-        self.driver.find_element_by_id('kw').click()
-        sleep(1)
-
         # input value
         self.driver.find_element_by_id('kw').send_keys(self.msg)
         sleep(1)
         self.driver.find_element_by_id('su').click()
-        sleep(3)
+        sleep(1)
     def tearDown(self):
         self.driver.close()
+
 if __name__ == "__main__":
     unittest.main()
-
