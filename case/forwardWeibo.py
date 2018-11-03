f8741e35ef9f7e4b6cf3799be366c469eb2b041d
AUTHENTICGIT <leogwork@gmail.com>
2018-11-02 22:20:56 -0400

    Auto-Forward Weibo · Selenium

--- case/forwardWeibo.py
+++ case/forwardWeibo.py
@@ -5,7 +5,7 @@ import os
 
 class Driver:
     def __init__(self):
-        self.driver = webdriver.Firefox()
+        self.driver = webdriver.Chrome()
         self.url = 'https://passport.weibo.cn/signin/login'
         self.account = '13093870429'
         self.pwd = 'Akira0429'
@@ -29,9 +29,9 @@ class Driver:
         sleep(3)
         # 定位第一条微博
         # self.driver.find_element_by_xpath("/html/body/div[1]/div[1]/div[2]/div[1]/div[2]/section/div[1]/div[2]/section").click()
-        self.driver.get("https://m.weibo.cn/status/4169399802125047")
+        self.driver.get("https://m.weibo.cn/status/4169032489925104")
         sleep(2)
-        for i in range(59,70):
+        for i in range(48,70):
             # 转发微博
             print(">>>打开转发框>>>")
             self.driver.find_element_by_xpath("/html/body/div/div[1]/div[2]/div/div/footer/div[1]").click()
