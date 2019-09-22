7b2d4e0f5ea39287de669a80b764000cd60e695d
AUTHENTICGIT <leogwork@gmail.com>
2019-09-22 20:01:11 +0800

    Build Database Record· MySQl

--- case/buildMySQL.py
+++ case/buildMySQL.py
@@ -13,7 +13,7 @@ class Mysql():
             self.cur1 = self.conn1.cursor()
             self.cur2 = self.conn2.cursor()
         except Exception as e:
-            print(e)
+            print("数据库连接错误！",e)
 
     def checkExist(self, t_name):
         sql = "SELECT table_name FROM tables WHERE table_schema = '{}' AND table_type LIKE '%table%' AND table_name = '{}'".format(self.dbname,t_name)
@@ -28,10 +28,33 @@ class Mysql():
             return 0
 
     def createTable(self):
-        pass
+        self.cur2.execute('''CREATE TABLE fake1
+        user_id INT PRIMARY KEY NOT NULL,
+        total_back_amount DECIMAL(10,2) NOT NULL,
+        nickname char(255) NOT NULL,
+        mail char(255) NOT NULL
+        ''')
+        print("Table [fake1] created successfully!")
 
-    def insertRecord(self):
-        pass
+    def insertRecord(self, t_name):
+        fake = Faker(locale='zh-CN')
+        sql = "INSERT INTO {} (user_id, total_back_amount, nickname, icon) VALUES (%s,%s,%s,%s) ON DUPLICATE KEY UPDATE total_back_amount = total_back_amount + values(total_back_amount);".format(t_name)
+        data = []
+        j = 0
+        try:
+            while j < 500:
+                data.append((
+                    str(fake.pyint(min_value=0,max_value=9999999,step=1)),
+                    fake.pydecimal(right_digits=2,min_value=0,max_value=10000),
+                    fake.user_name(),
+                    fake.image_url(width=None, height=None)
+                ))
+                j = j + 1
+            self.cur2.executemany(sql, data)
+            self.conn2.commit()
+            print("500 records inserted!")
+        except Exception as e:
+            print("插入记录失败！", e)
 
     def close(self):
         self.conn2.close()
@@ -46,6 +69,6 @@ if __name__ == '__main__':
         enter = input("Do you wanna build one? (Yes/No)")
         if enter == 'Yes':
             mysql.createTable()
-            print("built!")
+    mysql.insertRecord(t_name=tablename)
 
     mysql.close()
